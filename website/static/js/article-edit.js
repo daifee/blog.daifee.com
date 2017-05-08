87a6f57d44f5459f77171eeb092dbe4b3cada093
epooren <mr_zhangjiayong@163.com>
2017-05-08 23:47:12 +0800

    修改好了样式

--- website/static/js/article-edit.js
+++ website/static/js/article-edit.js
@@ -1,7 +1,16 @@
 $(function () {
+  var $editormd = $('#editormd');
+  var height = $(window).height();
+  var editorTop = $editormd[0].getBoundingClientRect().top;
+  var editorHeight = height - editorTop;
+
+  $editormd.parent().css('height', editorHeight);
+
+
   var editor = editormd("editormd", {
     path : "/static/js/editor/lib/", // Autoload modules mode, codemirror, marked... dependents libs path
-    height: 640,
-    toolbarIcons: 'simple'
+    height: '100%',
+    width: 'auto',
+    toolbarIcons: 'mini'
   });
 });
